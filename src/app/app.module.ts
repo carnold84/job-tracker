1b882e5c855673de8551575847382ed392e0f819
c-arnold <chris.wittyname@gmail.com>
2021-06-06 15:12:47 +1200

    Added demo data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { JobsModule } from './views/jobs/jobs.module';
+import api from './shared/services/demo-api';
+
+// included for the demo api
+// TODO: make conditional at build time when real api is added
+async function initializeApp() {
+  await api.init();
+}
 
 @NgModule({
   declarations: [AppComponent],
@@ -14,7 +21,13 @@ import { JobsModule } from './views/jobs/jobs.module';
     AppRoutingModule,
     JobsModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: () => initializeApp,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
   exports: [],
 })
